6298f0c24ccaf36c96d997309a54587e6206a8a5
Amos Maimon <amosmim@gmail.com>
2017-07-28 00:08:48 +0300

    start master

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import com.sun.org.apache.xalan.internal.utils.FeatureManager;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+	    MyActiveObject active = new MyActiveObject();
+	    active.addFunction("mul2", (Double x)->x*2);
+        active.addFunction("print", (Object x)->{
+            System.out.println(x.toString());
+            return x.toString();
+        });
+        active.addFunction("sqr", (Double x)->x*x);
+        active.addFunction("len", (String x)->x.length());
+        System.out.println(active.exec("len",
+                                active.exec("print",
+                                    active.exec("mul2",
+                                        active.exec("sqr",2.0)
+                                            .get()).get()).get()).get());
+
+        active.stop();
+        return;
     }
+
+
 }
